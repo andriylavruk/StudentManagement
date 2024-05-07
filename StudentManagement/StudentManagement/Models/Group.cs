d6f490a39600d9be8380fb3e305703de7f862c6e
andriylavruk <andriy.lavruk.ua@gmail.com>
2024-05-07 19:04:15 +0300

    Create Group and Student models.

---
+++ StudentManagement/StudentManagement/Models/Group.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace StudentManagement.Models;
+
+public class Group
+{
+    [DisplayName("Id")]
+    public int Id { get; set; }
+
+    [DisplayName("Name")]
+    [Required(ErrorMessage = "Group name is requerid")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "Group name must be between 3 and 50 characters")]
+    public string Name { get; set; } = string.Empty;
+}
---
+++ StudentManagement/StudentManagement/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.Models;
+
+public class Student
+{
+    [DisplayName("Id")]
+    public int Id { get; set; }
+
+    [DisplayName("Name")]
+    [Required(ErrorMessage = "Name is requerid")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+    public string Name { get; set; } = string.Empty;
+
+    [DisplayName("Surname")]
+    [Required(ErrorMessage = "Surname is requerid")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "Surname must be between 3 and 50 characters")]
+    public string Surname { get; set; } = string.Empty;
+
+    [DisplayName("GroupId")]
+    public int? GroupId { get; set; }
+
+    [DisplayName("Student group")]
+    public Group? StudentGroup { get; set; }
+}
